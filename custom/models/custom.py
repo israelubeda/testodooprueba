33ec6ff13c5907a8a4afe987ff9577c9a3398ad9
Mario Roberto <120336893+mrgomezsv@users.noreply.github.com>
2023-01-25 13:41:40 -0600

    Update custom.py

--- custom/models/custom.py
+++ custom/models/custom.py
@@ -29,6 +29,37 @@ class Purchase_order_custom(models.Model):
         #de la linea subo a la orden de compra
         #res.update(field_dato=self.order_id.field_l5)
         return res
+    
+    
+    #####################################################
+
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = 'purchase.order'
+    _description = 'Agrega campo L5 a la Información de Solicitudes de presupuesto'
+
+    habitat_partner_id2 = fields.Many2one(string="L5", related="ht_budget_tr_id.habitat_partner_id")
+
+class Purchase_order_custom(models.Model):
+    _inherit = 'purchase.order.line'
+    _description = 'creamos campo l5 y reescribimos funcion'
+
+    def _prepare_account_move_line(self, move=False):
+        current_uid = self.env.uid
+        user = self.env['res.users'].browse(current_uid)
+        res = super()._prepare_account_move_line(move)
+        res.update({"habitat_partner_id" : self.order_id.habitat_partner_id2,"habitat_branch_id" : user.branch_office_tr_id.id})
+        return res 
+
+
+    
+    
+    #####################################################
         
         
     
